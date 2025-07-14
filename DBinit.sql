54c60e72b9c4f5c6b9302c01ca0bf4cf5ae9429a
EthanWonCho <realethanwoncho@gmail.com>
2025-07-14 14:48:20 +0900

    sql init

---
+++ DBinit.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE modai;
+
+use modai;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
+
+CREATE TABLE RequestLogs (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  timestamp DATETIME NOT NULL,
+  ip VARCHAR(45) NOT NULL,
+  method VARCHAR(10) NOT NULL,
+  url TEXT NOT NULL,
+  userAgent TEXT,
+  referrer TEXT,
+  status INT NOT NULL,
+  contentLength INT NOT NULL
+);

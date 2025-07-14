5d2c4f738e0d77a2da7b51cc0940d41b0071bb5d
EthanWonCho <realethanwoncho@gmail.com>
2025-07-14 14:41:04 +0900

    dotenv prob

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import cookieParser from "cookie-parser";
 // import logger from "morgan";
-import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
 
 import indexRouter from "./routes/index.js";
--- utils/connectdb.js
+++ utils/connectdb.js
@@ -1,7 +1,5 @@
 import mysql from "mysql2/promise";
-import dotenv from 'dotenv/config';
-
-dotenv.config();
+import 'dotenv/config';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
